12d83f899a4539b1b3944dfd43981c4754f56f21
o-ishchenko <olya.galaychuk@gmail.com>
2022-02-11 18:30:08 +0200

    add styles

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,10 +1,11 @@
 import React from "react";
+import s from './FeedbackOptions.module.css'
 
 function FeedbackOptions({ options, onLeaveFeedback }) {
     return (
         <div>
           {options.map(option => (
-              <button key={option} onClick={() => onLeaveFeedback(option)}>{option}</button>
+              <button className={s.button} key={option} onClick={() => onLeaveFeedback(option)}>{option}</button>
           ))}
         </div>
     )
