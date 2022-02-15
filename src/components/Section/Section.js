ebfc3e2a2617b3135da290df4066141aec0806da
o-ishchenko <olya.galaychuk@gmail.com>
2022-02-15 12:06:22 +0200

    Добавляет правки и форматирование

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,16 +1,17 @@
-import React from "react";
-import s from './Section.module.css'
-import PropTypes from "prop-types";
+import React from 'react';
+import s from './Section.module.css';
+import PropTypes from 'prop-types';
 
-const Section = ({ title, children }) => 
-    <div className={s.container}>
-        <h1>{title }</h1>
-            {children}
-    </div>
-        
+const Section = ({ title, children }) => (
+  <div className={s.container}>
+    <h1>{title}</h1>
+    {children}
+  </div>
+);
 
 Section.propTypes = {
-    title: PropTypes.string.isRequired
-}
+  title: PropTypes.string.isRequired,
+  children:  PropTypes.arrayOf(PropTypes.node)
+};
 
-export default Section 
+export default Section;
